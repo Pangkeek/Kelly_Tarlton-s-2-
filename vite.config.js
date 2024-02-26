1d932094d47586c3909f6c98f222c87791cad99c
Pangkeek <142195531+Pangkeek@users.noreply.github.com>
2024-02-26 17:05:55 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    base: '/Kelly_Tarlton.github.io/',
+    base: '/Kelly_Tarlton-s-2-/',
     assetsInlineLimit: 0,
   },
   assetsDir: './assets/media',
